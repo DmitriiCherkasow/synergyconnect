e51977b461ab10f39ec9f4ece5792de3f88391ea
Dmitrii Cherkasow <Dmitrij.cherkasow@yandex.ru>
2026-08-24 15:03:22 +0400

    front 0.0.0

---
+++ frontend/vite.config.ts
@@ -0,0 +1,15 @@
+// @ts-ignore
+import { defineConfig } from 'vite'
+// @ts-ignore
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
